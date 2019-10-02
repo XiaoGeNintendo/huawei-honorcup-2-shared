84d6f128495bcb0ce6ad3836df19d5997417fb19
DamnXIZCM <44310508+DamnXIZCM@users.noreply.github.com>
2019-10-02 10:57:58 +0800

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -1,34 +1,43 @@
 from PIL import Image
 import numpy as np
-img=Image.open('D:\\Personal\\Workspace\\data_train\\64-sources\\1200.png')
-a=np.array(img)
-sz=int(input())
-sz_per_row=512//sz
-b=np.zeros([sz_per_row**2,sz,sz,3],dtype=np.int16)
-c=np.zeros([sz_per_row**2,sz,sz],dtype=float)
-for i in range(0,512,sz):
-    for j in range(0,512,sz):
-        for k in range(0,sz):
-            for l in range(0,sz):
-                for m in range(3):
-                    b[i//sz*sz_per_row+j//sz,k,l,m]=a[i+k,j+l,m]
+import sys
+import os
 
-for i in range(sz_per_row**2):
-    for j in range(sz):
-        for k in range(sz):
-            c[i,j,k]=b[i,j,k,0]*0.299 + b[i,j,k,1]*0.587 + b[i,j,k,2]*0.114
+if not os.path.isdir('.\\output\\'):
+    os.makedirs('.\\output\\')
 
+image_folder=sys.argv[1]
+start_image=int(sys.argv[2])
+end_image=int(sys.argv[3])
+sz=int(sys.argv[4])
 
+for nowimg in range(start_image,end_image+1):
 
-with open('D:\\Personal\\Workspace\\output.txt','w') as f:
-    for i in range(sz):
+    img=Image.open(image_folder+'\\'+str(nowimg)+'.png')
+    a=np.array(img)
+    #sz=int(input())
+    sz_per_row=512//sz
+    b=np.zeros([sz_per_row**2,sz,sz,3],dtype=np.int16)
+    c=np.zeros([sz_per_row**2,sz,sz],dtype=float)
+    for i in range(0,512,sz):
+        for j in range(0,512,sz):
+            for k in range(0,sz):
+                for l in range(0,sz):
+                    for m in range(3):
+                        b[i//sz*sz_per_row+j//sz,k,l,m]=a[i+k,j+l,m]
+
+    for i in range(sz_per_row**2):
         for j in range(sz):
-            f.write(str(c[32,i,j])+' ')
-        f.write('\n')
+            for k in range(sz):
+                c[i,j,k]=b[i,j,k,0]*0.299 + b[i,j,k,1]*0.587 + b[i,j,k,2]*0.114
 
-    f.write('\n'*20)
 
-    for i in range(sz):
-        for j in range(sz):
-            f.write(str(c[21,i,j])+' ')
-        f.write('\n')
+
+    with open('.\\output\\output'+str(nowimg)+'.txt','w') as f:
+        for i in range(sz_per_row**2):
+            for j in range(sz):
+                for k in range(sz):
+                    f.write(str(c[i,j,k])+' ')
+                f.write('\n')
+
+            f.write('\n')
